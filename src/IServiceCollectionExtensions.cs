d13cb35592bf94d4f3927dd4faf73dd1c0f53394
Derek Flenniken <derek.flenniken@ucsf.edu>
2020-02-25 16:12:33 -0800

    Use AspNetRole instead of IdentityRole

--- src/IServiceCollectionExtensions.cs
+++ src/IServiceCollectionExtensions.cs
@@ -13,7 +13,7 @@ namespace Microsoft.AspNetCore.Identity.AspNetMembershipAdapter
             services.AddDbContext<AspNetMembershipDbContext>(options => options.UseSqlServer(connectionString));
             services.AddTransient<IPasswordHasher<AspNetMembershipUser>, AspNetMembershipPasswordHasher>();
 
-            return services.AddIdentity<AspNetMembershipUser, IdentityRole>()
+            return services.AddIdentity<AspNetMembershipUser, AspNetRole>()
                 .AddEntityFrameworkStores<AspNetMembershipDbContext>()
                 .AddUserStore<AspNetMembershipUserStore>()
                 .AddDefaultTokenProviders();
@@ -24,7 +24,7 @@ namespace Microsoft.AspNetCore.Identity.AspNetMembershipAdapter
             services.AddDbContext<AspNetMembershipDbContext>(options => options.UseSqlServer(connectionString));
             services.AddTransient<IPasswordHasher<AspNetMembershipUser>, AspNetMembershipPasswordHasher>();
 
-            return services.AddIdentity<AspNetMembershipUser, IdentityRole>(setupAction)
+            return services.AddIdentity<AspNetMembershipUser, AspNetRole>(setupAction)
                 .AddEntityFrameworkStores<AspNetMembershipDbContext>()
                 .AddUserStore<AspNetMembershipUserStore>()
                 .AddDefaultTokenProviders();
